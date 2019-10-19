ae4907393ad366171d0a75bdc3e7a55ab7a07c86
Cindy <cindygu@umich.edu>
2019-10-19 16:27:42 -0400

    added all Rutgers players

--- Scrapers/rosters.py
+++ Scrapers/rosters.py
@@ -56,7 +56,7 @@ def main():
                                   password="LeBron>MJ!")
     cursor = cnx.cursor(buffered=True)
 
-    fileName = 'haileyschneden.htm'
+    fileName = 'alexalfano.htm'
     print fileName
     html = open(fileName).read()
     soup = BeautifulSoup(html, 'html.parser')
